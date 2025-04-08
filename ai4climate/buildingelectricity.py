f4309cc0d96516e8c656cddd526ac11c2e967269
Arsam Aryandoust <arsam@mit.edu>
2025-04-08 10:25:08 -0400

    implementing BuildingElectricity loading functionality

--- ai4climate/buildingelectricity.py
+++ ai4climate/buildingelectricity.py
@@ -10,8 +10,12 @@ from typing import Dict, Any, Tuple, Union, List
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 AVAIL_SUBTASKNAMES_LIST = [
-    'buildings_92',
-    'buildings_451'
+    'odd_time_buildings92',
+    'odd_space_buildings92',
+    'odd_spacetime_buildings92',
+    'odd_time_buildings451',
+    'odd_space_buildings451',
+    'odd_spacetime_buildings451'
 ]
 
 def load(
@@ -49,4 +53,34 @@ def load(
     if subtask_name not in AVAIL_SUBTASKNAMES_LIST:
         raise ValueError(f"Unknown subtask name: {subtask_name}")
 
+    # exdend local_dir with corresponding profiles
+    if subtask_name.endswith('92'):
+        local_dir = os.join.path(local_dir, 'profiles_92')
+    elif subtask_name.endswith('451'):
+        local_dir = os.join.path(local_dir, 'profiles_451')
+    else:
+        raise VallueError('Check subtask handling. Naming not consistent!')
+
+    # load electric profiles
+    df_loads = load_electric_load_profiles(local_dir)
+
     
+
+    subtask_data = {
+        'train_data': train_data,
+        'val_data': val_data,
+        'test_data': test_data
+    }
+    
+
+def load_electric_load_profiles(local_dir: str):
+    """
+    Load electric load profiles as DataFrame
+
+    """
+
+    # load load profiles
+    path_load = os.path.join(local_dir, 'load_profiles.csv')
+    df_loads = pd.read_csv(path_load)
+
+    return df_loads 
